e9cd8a5e9ecae8f620053b2af9d72b5bc9c623af
Ila Sahu <ilasahu@Ilas-MacBook-Pro.local>
2020-09-14 14:28:17 +0530

    prod build

---
+++ .next/static/gituqZkjUYca3yBuO7uBq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a,b,c){return {"/":[a,b,c],"/index":[a,b,c]}}("static\u002Fchunks\u002Fa9a7754c.28ea65b5f687917f41c6.js","static\u002Fchunks\u002Fcb1608f2.918565a2f0315c27a090.js","static\u002Fcss\u002Ff8dbcbcc69687c2bc319.css"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
