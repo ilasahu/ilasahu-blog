c3fc6b876d0b16b3987a50d495230f029338d1dd
Ila Sahu <ilasahu@Ilas-MBP.Dlink>
2020-11-11 12:56:55 +0530

    meta tags updated

--- components/layout.js
+++ components/layout.js
@@ -15,16 +15,15 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="Personal blog of ila sahu"
         />
         <meta
           property="og:image"
-          content={`https://og-image.now.sh/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          content="https://ilasahu.com/images/ila.png"
         />
+        <meta name="keyword" content="personal blog, software, technology, backend, php, laravel, developer, bangalore, development, application, fullstack, engineer" />
         <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:card" content="https://twitter.com/ilasahu94" />
       </Head>
       <header className={`${styles.header} ${styles.row} `}>
         <>
